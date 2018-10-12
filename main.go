57403706ff6ade6fd12fc5a800667e59cd03dece
Andrey Andreev <boompyz@gmail.com>
2018-10-13 00:49:15 +0100

    Getting once.

--- main.go
+++ main.go
@@ -1,10 +1,33 @@
 package main
 
 import (
-	"fmt"
+	"bufio"
+	"os"
+	"os/exec"
+	"strings"
 
 	"github.com/Boompyz/nyaa-si-watcher/torrentoptions"
 )
+
+func addTorrent(link string) {
+	err := exec.Command("deluge-console", "add", link).Start()
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func getWatches(fileName string) []string {
+	file, err := os.Open(fileName)
+	if err != nil {
+		panic("Couldn't read file")
+	}
+	scanner := bufio.NewScanner(file)
+	lines := make([]string, 0)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines
+}
 
 func main() {
 	options, err := torrentoptions.GetAllOptions()
@@ -12,8 +35,18 @@ func main() {
 		panic(err.Error())
 	}
 
+	watched := getWatches("watch/watching.txt")
 	for _, option := range options {
-		fmt.Println(option.GetId())
-		fmt.Println(option)
+		get := false
+		for _, watchedTitle := range watched {
+			if strings.Contains(option.Title, watchedTitle) {
+				get = true
+				break
+			}
+		}
+
+		if get {
+			addTorrent(option.Link)
+		}
 	}
 }
