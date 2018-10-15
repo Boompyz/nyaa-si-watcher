e4c3456e8f144cac8dc1c5d95bd5a663d08bc09e
Andrey Andreev <boompyz@gmail.com>
2018-10-15 21:20:33 +0100

    Forgot to parse flags...

--- main.go
+++ main.go
@@ -12,6 +12,8 @@ func main() {
 	var confDir = flag.String("confDir", "/var/lib/nyaa-si-watcher", "The directory to look for watching.conf and resolved.conf")
 	var resetResolved = flag.Bool("r", false, "Set this to reset already resolved files. Default is false.")
 
+	flag.Parse()
+
 	contentHandler := torrentoptions.NewContentHandler(*confDir)
 
 	if *resetResolved {
