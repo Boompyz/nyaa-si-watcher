777a2338281748bf56b131309895dff43eddf4d1
Andrey Andreev <boompyz@gmail.com>
2018-12-27 14:40:19 +0200

    Added new functionalities to the web interface.

--- webui/script.js
+++ webui/script.js
@@ -5,3 +5,10 @@ function request() {
     xmlrequest.send(text);
     alert('Sent request for ' + text);
 }
+function addWatch() {
+    text = document.getElementById("query").value;
+    xmlrequest = new XMLHttpRequest();
+    xmlrequest.open("POST", "/addwatch", true);
+    xmlrequest.send(text);
+    alert('Sent request for ' + text);
+}
--- webui/serv.go
+++ webui/serv.go
@@ -7,49 +7,21 @@ import (
 	"log"
 	"net/http"
 	"strconv"
-	"strings"
 	"time"
 
-	"github.com/Boompyz/nyaa-si-watcher/announcer"
-	"github.com/Boompyz/nyaa-si-watcher/torrentoptions"
+	"github.com/Boompyz/nyaa-si-watcher/store"
 )
 
 var confDir string
-var contentHandler *torrentoptions.ContentHandler
+var contentHandler *store.ContentHandler
 var tmpl *template.Template
 
 func update() {
-
-	options, err := torrentoptions.GetAllOptions()
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-
-	options = contentHandler.Filter(options)
-	for _, option := range options {
-		fmt.Println("Found new: " + option.Title)
-	}
-
-	announcer := announcer.NewMailAnnouncer(confDir)
-	announcer.Announce(options)
-
-	get(options)
-}
-
-func get(options []torrentoptions.TorrentOption) {
-
-	err := contentHandler.Get(options)
-	if err != nil {
-		panic(err.Error())
-	}
-	if len(options) != 0 {
-		fmt.Println("Added successfuly")
-	}
+	contentHandler.GetNew()
 }
 
 type pageData struct {
-	ContentHandler torrentoptions.ContentHandler
+	ContentHandler store.ContentHandler
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -63,11 +35,16 @@ func handler(w http.ResponseWriter, r *http.Request) {
 func requests(w http.ResponseWriter, r *http.Request) {
 	requestByte, _ := ioutil.ReadAll(r.Body)
 	request := string(requestByte)
-	strings.Replace(request, " ", "+", -1)
 
-	options, _ := torrentoptions.GetAllOptionsWithQuery(request)
-	fmt.Println(options)
-	get(options)
+	contentHandler.GetNewQuery(request)
+	fmt.Println(request)
+}
+
+func addWatch(w http.ResponseWriter, r *http.Request) {
+	requestByte, _ := ioutil.ReadAll(r.Body)
+	request := string(requestByte)
+
+	contentHandler.AddNewWatch(request)
 	fmt.Println(request)
 }
 
@@ -78,20 +55,21 @@ func ignore(w http.ResponseWriter, r *http.Request) {
 func Listen(_confDir string, _port int, interval int) {
 	tmpl = template.Must(template.ParseFiles("webui/template.html"))
 
+	confDir = _confDir
+	contentHandler = store.NewContentHandler(confDir)
+
 	go func() {
 		for range time.NewTicker(time.Second * time.Duration(interval)).C {
 			update()
 		}
 	}()
 
-	confDir = _confDir
-	contentHandler = torrentoptions.NewContentHandler(confDir)
-
 	fs := http.FileServer(http.Dir("webui"))
 
 	http.Handle("/script.js", fs)
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/request", requests)
+	http.HandleFunc("/addwatch", addWatch)
 	http.HandleFunc("/favicon.ico", ignore)
 
 	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(_port), nil))
